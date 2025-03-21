f8d699faaa18f50ded5d31709116933d61615611
coderfalse <christojm.us@gmail.com>
2025-03-21 16:31:58 +0530

    Update routing logic in RouteOptimizer to correctly calculate distances

--- forestfire/optimizer/services/routing.py
+++ forestfire/optimizer/services/routing.py
@@ -182,7 +182,7 @@ class RouteOptimizer:
         dist2_walkway = self.walkway_calculator.get_walkway_position(route[-1][1])
         
         point1 = tuple(picker_location) if not isinstance(picker_location, tuple) else picker_location
-        point2 = tuple((dist1_walkway, route[0][1])) if not isinstance((dist1_walkway, route[0][1]), tuple) else (dist1_walkway, route[0][1])
+        point2 = tuple((dist1_walkway, route[0][1])) if not isinstance((dist1_walkway, route[0][1]), tuple) else (dist2_walkway, route[0][1])
 
         dist1 = self.distance_calculator.euclidean_distance(
             point1, 
